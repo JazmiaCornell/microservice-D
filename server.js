a9d82e28a2ebf8c9bfd914e6074f98363fb57849
Jazmia Cornell <jazmiadc@gmail.com>
2025-09-05 20:48:56 -0400

    cleanup

--- server.js
+++ server.js
@@ -25,16 +25,6 @@ app.use(
   })
 );
 
-/*
-// database connection
-const db = mysql.createPool({
-  connectionLimit: process.env.DB_CONN_LIMIT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-}); */
-
 // database connection for users
 const db = mysql.createPool({
   connectionLimit: 10,
